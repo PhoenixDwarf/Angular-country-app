2fed2cfd70e5ae5a4c6837b9b587ceed9583c9fd
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-03-31 19:53:18 -0500

    Feat - Added mappers, interfaces and http calls

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { ListComponent } from '../../components/list/list.component';
 import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,9 +12,25 @@ import { CountryService } from '../../services/country.service';
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
 
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<Country[]>([]);
+
   handleOnsearch(query: string) {
+    if (this.isLoading()) return;
+    this.isLoading.set(true);
+    this.isError.set(null);
+
     this.countryService.searchByCapital(query).subscribe({
-      next: (res) => console.log(res),
+      next: (countries) => {
+        this.isLoading.set(false);
+        this.countries.set(countries);
+        console.log(this.countries());
+      },
+      error: (error) => {
+        this.isLoading.set(true);
+        this.isError.set(error);
+      },
     });
   }
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { RESTCountry } from '../interfaces/rest-countries.interfaces';
+import { map, Observable } from 'rxjs';
+import { CountryMapper } from '../mappers/country.mapper';
+import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -9,9 +13,10 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
   private readonly http = inject(HttpClient);
 
-  searchByCapital(query: string) {
+  searchByCapital(query: string): Observable<Country[]> {
     query = query.toLocaleLowerCase();
-
-    return this.http.get(`${API_URL}/capital/${query}`);
+    return this.http
+      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
+      .pipe(map(CountryMapper.mapRestCountryArrayToCountryArray));
   }
 }
